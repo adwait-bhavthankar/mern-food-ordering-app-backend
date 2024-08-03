4ee05ec4aa40de84b3f64743a88c31299c26d1ab
Adwait Bhavthankar <adwaitbhav4@gmail.com>
2024-08-03 16:28:34 +0530

    adding manage restaurant feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 
-
 const handleValidationErrors = async (
   req: Request,
   res: Response,
@@ -14,14 +13,39 @@ const handleValidationErrors = async (
   next();
 };
 
-
 export const validateMyUserRequest = [
-    body("name").isString().notEmpty().withMessage("Name must be a string"),
-    body("addressLine1")
+  body("name").isString().notEmpty().withMessage("Name must be a string"),
+  body("addressLine1")
     .isString()
     .notEmpty()
     .withMessage("AddressLine1 must be a string"),
   body("city").isString().notEmpty().withMessage("City must be a string"),
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
-  ];
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required."),
+  body("city").notEmpty().withMessage("City name is required."),
+  body("country").notEmpty().withMessage("Country name is required."),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a possitive number."),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a possitive integer."),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty."),
+  body("menuItems").isArray().withMessage("Menu items must be an array."),
+  body("menuItems.*.name")
+    .notEmpty()
+    .withMessage("menu item name is required."),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("menu item price is required and must be a possitive number."),
+  handleValidationErrors,
+];
