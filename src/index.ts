dc885a5ef0ff402bb2b04b403d8af2dca118f2fc
Adwait Bhavthankar <adwaitbhav4@gmail.com>
2024-08-10 18:33:38 +0530

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to database!");
 });
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDNARY_API_SECRET,
-})
+});
 
 const app = express();
 app.use(express.json());
@@ -26,7 +26,8 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", MyUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute)
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
